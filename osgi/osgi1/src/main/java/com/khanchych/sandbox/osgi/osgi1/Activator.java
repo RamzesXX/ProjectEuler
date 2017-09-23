b7a89a4017f14b375c33f4118137448d589cce3c
Roman Khanchych <roman.khanchych@nixsolutions.com>
2017-09-23 21:19:21 +0300

    Updates .gitignore Creates osgi.Activator

---
+++ osgi/osgi1/src/main/java/com/khanchych/sandbox/osgi/osgi1/Activator.java
@@ -0,0 +1,14 @@
+package com.khanchych.sandbox.osgi.osgi1;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+public class Activator implements BundleActivator {
+    public void start(BundleContext bundleContext) throws Exception {
+
+    }
+
+    public void stop(BundleContext bundleContext) throws Exception {
+
+    }
+}
