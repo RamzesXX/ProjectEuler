104b193afd8c7872d9f297e10df9fa53a7cba2b3
Roman Khanchych <roman.khanchych@nixsolutions.com>
2018-01-16 09:42:13 +0200

    Adds lengthOfLongestSubstring's test

--- algorithmicproblems/prometheus/src/main/java/com/khanchych/sandbox/algorithmic_problems/prometheus/KotsubaMethod.java
+++ algorithmicproblems/prometheus/src/main/java/com/khanchych/sandbox/algorithmic_problems/prometheus/KotsubaMethod.java
@@ -1,12 +1,25 @@
 package com.khanchych.sandbox.algorithmic_problems.prometheus;
 
+import java.math.BigInteger;
+
 public class KotsubaMethod {
+    private int count;
+    private String AD_BC;
+
+    public KotsubaMethod(String ab_cd){
+        this.count = 0;
+        this.AD_BC = ab_cd;
+    }
     public String mul(String first, String second) {
-        int n = first.length();
+        int n = Math.max(first.length(), second.length());
         int n2 = n / 2;
 
+
+        first = makeZeroString(n - first.length()) + first;
+        second = makeZeroString(n - second.length()) + second;
+
         if (n == 1) {
-            return String.valueOf((first.charAt(0) - '0')* (second.charAt(0) - '0'));
+            return String.valueOf((first.charAt(0) - '0') * (second.charAt(0) - '0'));
         }
 
         String a = first.substring(0, n2);
@@ -15,53 +28,32 @@ public class KotsubaMethod {
         String d = second.substring(n2);
         String ac = mul(a, c);
         String bd = mul(b, d);
+        String ab_cd = mul(add(a, b), add(c, d));
+        String ad_bc = sub(ab_cd, add(ac, bd));
+
         String result;
 
-        result = ac + makeZeroString(n);
-        result = add(result, add(mul(add(a, b), add(c, d)), neg(add(ac, bd))) + makeZeroString(n2));
+        if (AD_BC.equals(ad_bc)){
+            count++;
+        }
+        result = ac + makeZeroString(2 * (n - n2));
+        result = add(result, ad_bc + makeZeroString(n - n2));
         result = add(result, bd);
-
         return result;
     }
 
-    protected String neg(String first) {
-        if (first.charAt(0) == '-') {
-            return first.substring(1);
-        }
+    protected String add(String first, String second) {
+        BigInteger firstBI = new BigInteger(first);
+        BigInteger secondBI = new BigInteger(second);
 
-        return '-' + first;
+        return firstBI.add(secondBI).toString();
     }
 
-    protected String add(String first, String second) {
-        boolean isSubstraction = (first.charAt(0) == '-') ^ (second.charAt(0) == '-');
-        int overflow;
-        StringBuilder firstSB;
-        StringBuilder secondSB;
-        StringBuilder result = new StringBuilder(first.length() + second.length() + 1);
+    protected String sub(String first, String second) {
+        BigInteger firstBI = new BigInteger(first);
+        BigInteger secondBI = new BigInteger(second);
 
-        if (first.length() < second.length()) {
-            firstSB = new StringBuilder(first).reverse();
-            secondSB = new StringBuilder(second).reverse();
-        } else {
-            secondSB = new StringBuilder(first).reverse();
-            firstSB = new StringBuilder(second).reverse();
-        }
-
-        overflow = 0;
-        for (int i = 0; i < firstSB.length(); i++) {
-            overflow += firstSB.charAt(i) - '0';
-            if (i < secondSB.length()) {
-                overflow += secondSB.charAt(i) - '0';
-            }
-            result.append((char)('0' + overflow % 10));
-            overflow /= 10;
-        }
-
-        if (overflow > 0) {
-            result.append((char)('0' + overflow % 10));
-        }
-
-        return result.reverse().toString();
+        return firstBI.subtract(secondBI).toString();
     }
 
     private String makeZeroString(int n) {
@@ -73,4 +65,16 @@ public class KotsubaMethod {
 
         return result.toString();
     }
+
+    public int getCount() {
+        return count;
+    }
+    public static void main(String[] args) {
+        KotsubaMethod kotsubaMethod = new KotsubaMethod("12");
+        String first = "1685287499328328297814655639278583667919355849391453456921116729";
+        String second = "7114192848577754587969744626558571536728983167954552999895348492";
+        System.out.println(kotsubaMethod.mul(first,second));
+        System.out.println(kotsubaMethod.getCount());
+    }
+
 }
--- algorithmicproblems/prometheus/src/test/java/com/khanchych/sandbox/algorithmic_problems/prometheus/KotsubaMethodTest.java
+++ algorithmicproblems/prometheus/src/test/java/com/khanchych/sandbox/algorithmic_problems/prometheus/KotsubaMethodTest.java
@@ -3,7 +3,7 @@ package com.khanchych.sandbox.algorithmic_problems.prometheus;
 import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 public class KotsubaMethodTest {
 
@@ -11,7 +11,7 @@ public class KotsubaMethodTest {
 
     @Before
     public void setUp() throws Exception {
-        kotsubaMethod = new KotsubaMethod();
+        kotsubaMethod = new KotsubaMethod("");
     }
 
     @Test
@@ -24,6 +24,34 @@ public class KotsubaMethodTest {
     }
 
     @Test
+    public void mul3() {
+        String first = "216";
+        String second = "133";
+        String result = "28728";
+
+        assertEquals(result, kotsubaMethod.mul(first, second));
+    }
+
+    @Test
+    public void mul_1234_5678() {
+        String first = "1234";
+        String second = "5678";
+        int result = 1;
+        kotsubaMethod = new KotsubaMethod("3");
+        kotsubaMethod.mul(first, second);
+        assertEquals(result, kotsubaMethod.getCount());
+    }
+
+    @Test
+    public void mulBig() {
+        String first = "21625695688898558125310188636840316594920403182768";
+        String second = "13306827740879180856696800391510469038934180115260";
+        String result = "287769407308846640970310151509826255482575362419155842891311909556878670000425352112987881085839680";
+
+        assertEquals(result, kotsubaMethod.mul(first, second));
+    }
+
+    @Test
     public void add() {
         String first = "11";
         String second = "12";
@@ -31,6 +59,7 @@ public class KotsubaMethodTest {
 
         assertEquals(result, kotsubaMethod.add(first, second));
     }
+
     @Test
     public void addWithOverflow() {
         String first = "55";
@@ -39,6 +68,7 @@ public class KotsubaMethodTest {
 
         assertEquals(result, kotsubaMethod.add(first, second));
     }
+
     @Test
     public void addDifferentLength() {
         String first = "5555";
@@ -46,5 +76,32 @@ public class KotsubaMethodTest {
         String result = "5620";
 
         assertEquals(result, kotsubaMethod.add(first, second));
+    }
+
+    @Test
+    public void sub() {
+        String first = "15";
+        String second = "12";
+        String result = "3";
+
+        assertEquals(result, kotsubaMethod.sub(first, second));
+    }
+
+    @Test
+    public void subWithOverflow() {
+        String first = "55";
+        String second = "65";
+        String result = "-10";
+
+        assertEquals(result, kotsubaMethod.sub(first, second));
+    }
+
+    @Test
+    public void subDifferentLength() {
+        String first = "5555";
+        String second = "65";
+        String result = "5490";
+
+        assertEquals(result, kotsubaMethod.sub(first, second));
     }
 }
